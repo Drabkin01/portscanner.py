5386ca597e9c1f33e53fb217b33ab56bc9c7eb2a
Liam Drabkin <64532635+Drabkin01@users.noreply.github.com>
2023-06-07 10:41:02 +0200

    Update portscanner.py

--- portscanner.py
+++ portscanner.py
@@ -2,7 +2,7 @@
 
 import socket
 
-
+#Function to scan a specfic target for open ports
 def scan(target, ports):
 	print('\n' + ' Starting Scan For ' + str(target))
 	for port in range(1,ports):
@@ -18,9 +18,11 @@ def scan_port(ipaddress, port):
 	except:
 		pass
 
-
+# get the targets and ports from the user.
 targets = input("[*] Enter Targets To Scan(split them by ,): ")
 ports = int(input("[*] Enter How Many Ports You Want To Scan: "))
+
+#check if multiple targets are provided by user
 if ',' in targets:
 	print(termcolor.colored(("[*] Scanning Multiple Targets"), 'green'))
 	for ip_addr in targets.split(','):
